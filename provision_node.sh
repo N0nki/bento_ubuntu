adf6426a403ae2d31834c7febc15b5c77e4ef59e
N0nki <rgstr_land@icloud.com>
2017-03-06 04:03:37 +0900

    add rbenv and pyenv

--- provision_node.sh
+++ provision_node.sh
@@ -7,7 +7,28 @@ echo "clone dotfiles"
 cd ~/
 git clone https://github.com/N0nki/dotfiles
 
+echo "setup vim"
+sh dotfiles/vim/setup_vim.sh
+
 echo "setup neovim"
 mkdir ~/.config
-cd dotfiles/nvim
-sh ./setup_nvim.sh
+sh dotfile/nvim/setup_nvim.sh
+
+echo "Ruby"
+echo "rbenv"
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+~/.rbenv/bin/rbenv init
+srouce .bashrc
+type rbenv
+echo "ruby-build"
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+echo "Python"
+echo "pyenv"
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+exec $SHELL
