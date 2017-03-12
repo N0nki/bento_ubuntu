ab5bff8ef7f7563b5ca86eab5952000f02df4efa
N0nki <rgstr_land@icloud.com>
2017-03-13 02:25:14 +0900

    update

--- provision_node.sh
+++ provision_node.sh
@@ -31,7 +31,8 @@ git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-exec $SHELL
+source .bashrc
+# exec $SHELL
 
 echo "ns3"
 cd
